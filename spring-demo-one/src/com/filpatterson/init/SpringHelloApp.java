a94750897109534edc3a5e2a02397aa6f1e96971
filpatterson <filpatterson10@gmail.com>
2020-07-15 12:42:42 +0300

    adding custom bean

--- spring-demo-one/src/com/filpatterson/init/SpringHelloApp.java
+++ spring-demo-one/src/com/filpatterson/init/SpringHelloApp.java
@@ -5,11 +5,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SpringHelloApp {
 
 	public static void main(String[] args) {
-		//	load the spring config file
+		//	load the spring configuration file
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		//	retrieve bean from spring container
 		Coach theCoach = context.getBean("myCoach", Coach.class);
+		//	use interface for casting object types
+		Coach anotherCoach = context.getBean("anotherCoach", Coach.class);
 		
 		//	call methods of bean
 		System.out.println(theCoach.getDailyWorkout());
