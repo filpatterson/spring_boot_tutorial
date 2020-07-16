426cad1bdbb2e130a62ebf32f1931619cffd3df9
filpatterson <filpatterson10@gmail.com>
2020-07-16 10:00:13 +0300

    adding several strings for fortune

--- spring-demo-one/src/com/filpatterson/init/HappyFortuneService.java
+++ spring-demo-one/src/com/filpatterson/init/HappyFortuneService.java
@@ -1,8 +1,17 @@
 package com.filpatterson.init;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService {
+	
+	private String[] arrayOfGreetings = {"hello, this is the first greeting!",
+			"well, you reached second greeting",
+			"ok, this is the last one"};
+	
+	private Random random = new Random();
+	
 	@Override
 	public String getFortune() {
-		return "today is your lucky day!";
+		return arrayOfGreetings[random.nextInt(3)];
 	}
 }
