85b0081031209efb9027cfb7bdce9b2537952df2
filpatterson <filpatterson10@gmail.com>
2020-07-23 16:42:26 +0300

    adding mvc project

--- spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="ISO-8859-1">
-<title>Main menu</title>
-</head>
+
 <body>
 
 	<h2> this is the main menu page </h2>
 
 </body>
+
 </html>
--- spring-mvc-demo/src/com/filpatterson/demo/mvc/HomeController.java
+++ spring-mvc-demo/src/com/filpatterson/demo/mvc/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Component
 public class HomeController {
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping("/")
 	public String showPage(HttpServletResponse httpResponse) {
 		return "main-menu";
 	}
