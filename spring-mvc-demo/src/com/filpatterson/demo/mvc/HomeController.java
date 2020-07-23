933b227751549c988e0f9b17a1f09f5a6ea33736
filpatterson <filpatterson10@gmail.com>
2020-07-23 16:33:35 +0300

    adding spring mvc project

--- spring-mvc-demo/src/com/filpatterson/demo/mvc/HomeController.java
+++ spring-mvc-demo/src/com/filpatterson/demo/mvc/HomeController.java
@@ -1,12 +1,15 @@
 package com.filpatterson.demo.mvc;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Component
 public class HomeController {
-	@RequestMapping("/")
-	public String showPage() {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String showPage(HttpServletResponse httpResponse) {
 		return "main-menu";
 	}
 }
