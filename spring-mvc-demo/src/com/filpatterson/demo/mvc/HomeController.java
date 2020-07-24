5ce763f2edd0927726b0ddf53226e942c90f4dda
filpatterson <filpatterson10@gmail.com>
2020-07-24 17:43:19 +0300

    fixing mvc connection to tomcat (required eclipse reinstall)

--- spring-mvc-demo/src/com/filpatterson/demo/mvc/HomeController.java
+++ spring-mvc-demo/src/com/filpatterson/demo/mvc/HomeController.java
@@ -1,15 +1,12 @@
 package com.filpatterson.demo.mvc;
 
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-@Component
+@Controller
 public class HomeController {
 	@RequestMapping("/")
-	public String showPage(HttpServletResponse httpResponse) {
-		return "main-menu";
+	public String showPage() {
+		return "main_menu";
 	}
 }
