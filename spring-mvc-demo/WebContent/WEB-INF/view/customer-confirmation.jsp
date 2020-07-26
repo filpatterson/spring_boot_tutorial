a254c050d97247124ef463e7c835ff2cbc6fdb4e
filpatterson <filpatterson10@gmail.com>
2020-07-26 16:55:32 +0300

    not null amount of passes, commenting some decisions

--- spring-mvc-demo/WebContent/WEB-INF/view/customer-confirmation.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/customer-confirmation.jsp
@@ -8,6 +8,7 @@
 <body>
 
 	Confirmed that you're: ${customer.firstName } ${customer.lastName } with passes = ${customer.freePasses }
+	Customer's postal code is ${customer.postalCode }
 
 </body>
 
--- spring-mvc-demo/src/com/filpatterson/demo/mvc/Customer.java
+++ spring-mvc-demo/src/com/filpatterson/demo/mvc/Customer.java
@@ -3,6 +3,7 @@ package com.filpatterson.demo.mvc;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -14,9 +15,15 @@ public class Customer {
 //	@NotBlank(message="is required")	//	this annotation also protects from whitespaces
 	private String lastName;
 	
+	//	using not null annotation makes required use of Integer, not int
+	@NotNull(message="is required")
 	@Min(value=0, message="must be greater than 0")
 	@Max(value=10, message="must be less than 10")
-	private int freePasses;
+	private Integer freePasses;
+	
+	//	pattern for getting only 5 digits or characters by postal code
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 characters or digits")
+	private String postalCode;
 
 	public String getFirstName() {
 		return firstName;
@@ -34,13 +41,19 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
 
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
-	
-	
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
 }
